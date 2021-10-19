e5a442bbc7a81fe7cafcbc575fbf23543b86c8e2
Maria Vostrikova <vostrikova.maria2017@yandex.ru>
2021-10-19 22:28:22 +0300

    tests added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,29 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldCalculateAmountToReceiveCashback() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 950;
+        int expected = cashbackHackService.remain(amount);
+        int actual = 50;
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void shouldCalculateAmountToReceiveCashbackIf1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int expected = cashbackHackService.remain(amount);
+        int actual = 0;
+        assertEquals(actual, expected);
+
+    }
+}
+
+
